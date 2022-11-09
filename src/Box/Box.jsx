53b9c51f2443e326b8623088188c74f155c2b5c8
Khaled Tanveer <khaledtanveer@hscs-mba-1753.lan>
2022-11-08 23:04:09 -0500

    new commit

--- src/Box/Box.jsx
+++ src/Box/Box.jsx
@@ -4,7 +4,9 @@ import "./Box.css"
 
 function Box({color}) {
     return (
+        <div>
         <div className="box" style={{background: color}}>The {color} Box</div>
+         <button className="button">lock me</button></div>
     );
 }
 
--- src/TextBox/TextBox.jsx
+++ src/TextBox/TextBox.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, createContext } from 'react'
+import { useState } from 'react'
 import "./TextBox.css"
 import Box from "../Box/Box"
 
